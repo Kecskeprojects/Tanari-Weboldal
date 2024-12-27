1c6718591c8d66a887527844653d2e2d767dc1df
Kecskeprojects <kirsch.a.p@gmail.com>
2024-12-27 03:36:08 +0100

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -16,6 +16,7 @@ export default function getRoutes() {
 	});
 
 	//Todo: Rewrite Controllers, so that database processes happen in new classes, on the Repository level
+	//Todo: Figure out https in IIS and make it work for the site in localhost
 	routers.get('/File/GetAll/', FileController.getAll);
 	routers.get('/File/GetRecent/', FileController.getRecent);
 	routers.get('/File/GetBySearchResult/', FileController.getBySearchResult);
